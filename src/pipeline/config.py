8105663061e92ea686de2c0d51a1c5c84ad91aa4
Mikhail Krivonosov <mike_live@mail.ru>
2020-11-23 18:06:39 +0300

    Added computation of correlations and metrics at once. Config updated

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -4,14 +4,24 @@ import os
 print(os.getcwd())
 
 correlations = {
-    'work_dir': Path(r'../../../data/SST/All2019_6h_0.75resolution'),
-    'input_file_name': 'resulting_cube_All2019_6h_0.75resolution.npz',
+    'work_dir': Path(r'../../../data/ERA5/ERA5_SST_1982_2019_3h_0.75'),
+    'input_file_name': 'resulting_cube_after_preproc_ERA5_SST_1982_2019_3h_0.75.npz',
     'input_var_name': 'arr_0',
-    'output_correlation_file_name': 'corr_online_All2019_6h_resolution_0.75_window_15d_delay_7d.npy',
+    'output_correlation_file_name': 'corr_online_preproc_ERA5_SST_1982_2019_3h_0.75_window_10d_delay_0d.npy',
 #    'output_correlation_var_name': 'result',
     'delay_time': 0,
-    'window_size': 2,
-    'num_threads': 2,
+    'window_size': 80,
+    'num_threads': 5,
+    'need_save': False,
+    'num_parts': 666,
+    'id_part': 1,
+    'output_level': 1,
+}
+
+network_metrics = {
+    'num_threads': 1,
+    'work_dir': correlations['work_dir'],
+    'output_metrics_file_name': 'metrics_corr_preproc_ERA5_SST_1982_2019_3h_0.75_window_10d_delay_0d.npy',
 }
 
 debug_level = 1
--- src/pipeline/pipeline.py
+++ src/pipeline/pipeline.py
@@ -10,10 +10,30 @@ def download_data(config):
 
 def make_corr_networks(config):
     from corr_network.corr_network import make_correlation_matricies
-    make_correlation_matricies(config)
+    return make_correlation_matricies(config)
 
-def compute_metrics(config):
+def compute_metrics(config, corr_networks):
+    corr_networks = np.moveaxis(corr_networks, 2, 0)
+    for i in range(corr_networks.shape[0]):
+        corr_network = corr_networks[i]
+        #metrics = compute_network_metrics(config, corr_network)
+    
     pass
+
+def compute_metrics_by_parts(config):
+    from tqdm import tqdm
+    from network_metrics.compute_metrics import parallel_compute_metrics
+    import numpy as np
+    metrics = []
+    for id_part in tqdm(range(1)): # config.correlations['num_parts']
+        config.correlations['id_part'] = id_part
+        corr_networks = make_corr_networks(config)
+        corr_networks = np.moveaxis(corr_networks, -1, 0)
+        metrics += parallel_compute_metrics(config, corr_networks[-2:])
+    print(len(metrics))
+    print(metrics[0])
+    metrics_file_name = config.network_metrics['work_dir'] / config.network_metrics['output_metrics_file_name']
+    np.save(metrics_file_name, metrics)
 
 def plot_2d_metrics(config):
     pass
@@ -28,6 +48,9 @@ def parse_args():
     parser.add_argument('--compute_correlations', dest='need_corr_network', action='store_const',
                     const=True, default=False,
                     help='compute correlation matricies')
+    parser.add_argument('--compute_correlations_and_metrics', dest='need_correlations_and_metrics', action='store_const',
+                    const=True, default=False,
+                    help='plot network metrics')
     parser.add_argument('--compute_metrics', dest='need_metrics', action='store_const',
                     const=True, default=False,
                     help='compute network metrics')
@@ -49,6 +72,9 @@ def main():
 
     if args.need_metrics:
         compute_metrics(config)
+
+    if args.need_correlations_and_metrics:
+        compute_metrics_by_parts(config)
 
     if args.need_plot:
         plot_2d_metrics(config)
