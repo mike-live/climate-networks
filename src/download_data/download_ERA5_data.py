a879b707117f7b80e4e83699bd5fac6413338584
VershininaOlga <olya.olya-ver@yandex.ru>
2021-02-26 17:45:58 +0300

    Added creation of a working directory into which data is loaded

--- src/download_data/download_ERA5_data.py
+++ src/download_data/download_ERA5_data.py
@@ -46,48 +46,54 @@ def preprocessing_data(download_ERA5_options, resulting_cube, latitude, longitud
         print('Done\n')
     
 
-def download_and_preprocessing_ERA5_data(download_ERA5_options):
-    start_year = download_ERA5_options['start_year']
-    end_year = download_ERA5_options['end_year']
+def download_and_preprocessing_ERA5_data(config):
+    config.download_ERA5_options['work_dir'].mkdir(parents=True, exist_ok=True)
+
+    start_year = config.download_ERA5_options['start_year']
+    end_year = config.download_ERA5_options['end_year']
     middle_year = (start_year + end_year) // 2
     
     print('Downloading data from cds.climate.copernicus.eu...')
     # download the first part
-    download_ERA5_options['end_year'] = middle_year
-    file_name_1st = download_ERA5_options['work_dir'] / (download_ERA5_options['name_var'] + '_' +
-                                                         str(start_year) + '_' + str(middle_year) + '.nc')
-    download_raw_data(download_ERA5_options, file_name_1st)
+    config.download_ERA5_options['end_year'] = middle_year
+    file_name_1st = config.download_ERA5_options['work_dir'] / (config.download_ERA5_options['name_var'] + '_' +
+                                                                str(start_year) + '_' + str(middle_year) + '.nc')
+    download_raw_data(config.download_ERA5_options, file_name_1st)
     
     # download the second part
-    download_ERA5_options['start_year'] = middle_year + 1
-    download_ERA5_options['end_year'] = end_year
-    file_name_2nd = download_ERA5_options['work_dir'] / (download_ERA5_options['name_var'] + '_' +
-                                                         str(middle_year + 1) + '_' + str(end_year) + '.nc')
-    download_raw_data(download_ERA5_options, file_name_2nd)
-    download_ERA5_options['start_year'] = start_year
+    config.download_ERA5_options['start_year'] = middle_year + 1
+    config.download_ERA5_options['end_year'] = end_year
+    file_name_2nd = config.download_ERA5_options['work_dir'] / (config.download_ERA5_options['name_var'] + '_' +
+                                                                str(middle_year + 1) + '_' + str(end_year) + '.nc')
+    download_raw_data(config.download_ERA5_options, file_name_2nd)
+    config.download_ERA5_options['start_year'] = start_year
     print('Done\n')
     
     # npz cube formation
     print('Forming the resulting data cube...')
     dset_1 = xr.open_dataset(str(file_name_1st))
-    data_cube_1 = read_nc.get_data_cube(dset_1, download_ERA5_options['name_var'])
+    data_cube_1 = read_nc.get_data_cube(dset_1, config.download_ERA5_options['name_var'])
     dset_2 = xr.open_dataset(str(file_name_2nd))
-    data_cube_2 = read_nc.get_data_cube(dset_2, download_ERA5_options['name_var'])
+    data_cube_2 = read_nc.get_data_cube(dset_2, config.download_ERA5_options['name_var'])
 
     longitude = read_nc.get_longitude(dset_1, 'longitude')
     print("n_longitude = ", len(longitude))
     latitude = read_nc.get_latitude(dset_1, 'latitude')
     print("n_latitude = ", len(latitude))
-    np.savetxt(download_ERA5_options['work_dir'] / download_ERA5_options['lon_file_name'], longitude, fmt='%.2f')
-    np.savetxt(download_ERA5_options['work_dir'] / download_ERA5_options['lat_file_name'], latitude, fmt='%.2f')
+    np.savetxt(config.download_ERA5_options['work_dir'] / config.download_ERA5_options['lon_file_name'],
+               longitude, fmt='%.2f')
+    np.savetxt(config.download_ERA5_options['work_dir'] / config.download_ERA5_options['lat_file_name'],
+               latitude, fmt='%.2f')
     
-    times = read_nc.form_times(download_ERA5_options)
+    times = read_nc.form_times(config.download_ERA5_options)
     print("n_times = ", len(times))
-    np.savetxt(download_ERA5_options['work_dir'] / download_ERA5_options['times_file_name'], times, fmt='%s')
+    np.savetxt(config.download_ERA5_options['work_dir'] / config.download_ERA5_options['times_file_name'],
+               times, fmt='%s')
     
     resulting_cube = read_nc.form_resulting_data_cube_from_parts_by_time(data_cube_1, data_cube_2)
     print('resulting_cube.shape = ', resulting_cube.shape)
-    np.savez(download_ERA5_options['work_dir'] / download_ERA5_options['res_cube_file_name'], resulting_cube)
+    np.savez(config.download_ERA5_options['work_dir'] / config.download_ERA5_options['res_cube_file_name'],
+             resulting_cube)
     print('Done\n')
     
-    preprocessing_data(download_ERA5_options, resulting_cube, latitude, longitude, times)
+    preprocessing_data(config.download_ERA5_options, resulting_cube, latitude, longitude, times)
