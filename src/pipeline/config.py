b361718c6db4f74d26b5a292434af899810d08c7
Mikhail Krivonosov <mike_live@mail.ru>
2021-04-27 15:36:55 +0300

    Updated configurations. Added project_path

--- src/pipeline/config.py
+++ src/pipeline/config.py
@@ -1,46 +1,8 @@
 from pathlib2 import Path
-
-import os
-print(os.getcwd())
-
-correlations = {
-    #'work_dir': Path(r'../../../data/ERA5/ERA5_MSLP_1982_2019_3h_0.75'),
-    'work_dir': Path(r'ERA5/ERA5_MSLP_1982_2019_3h_0.75'),
-    'input_file_name': 'resulting_cube_land_masked_after_preproc_ERA5_MSLP_1982_2019_3h_0.75.npz',
-    'input_var_name': 'arr_0',
-    'output_correlation_file_name': 'corr_online_land_masked_after_preproc_ERA5_MSLP_1982_2019_3h_0.75_window_10d_delay_0d.npy',
-#    'output_correlation_var_name': 'result',
-    'delay_time': 0,
-    'window_size': 80,
-    'num_threads': 85,
-    'need_save': False,
-    'num_parts': 666,
-    'id_part': 1,
-    'output_level': 1,
-}
-
-metrics = {
-    'work_dir': correlations['work_dir'],
-    'output_metrics_dir': 'metrics_corr_land_masked_after_preproc_ERA5_MSLP_1982_2019_3h_0.75_window_10d_delay_0d',
-    #'output_metrics_file_name': 'metrics_corr_land_masked_after_preproc_ERA5_MSLP_1982_2019_3h_0.75_window_10d_delay_0d.npy',
-    'metric_names_file_name': 'metric_names.npy',
-}
-
-network_metrics = {
-    'num_threads': 84,
-    'work_dir': correlations['work_dir'] / metrics['output_metrics_dir'],
-    #'output_metrics_file_name': 'metrics_corr_land_masked_after_preproc_ERA5_MSLP_1982_2019_3h_0.75_window_10d_delay_0d.npy',
-    'output_network_metrics_dir': Path('network_metrics'),
-}
-
-diff_metrics = {
-    'num_threads': 2,
-    'work_dir': correlations['work_dir'] / metrics['output_metrics_dir'],
-    'output_diff_metrics_dir': Path('diff_metrics'),
-}
+from config_project import project_data_dir
 
 download_ERA5_options = {
-    'work_dir': Path(r'ERA5/ERA5_MSLP_1982_2019_3h_0.75'),
+    'work_dir': project_data_dir / Path(r'ERA5/ERA5_MSLP_1982_2019_3h_0.75'),
     'lat_file_name': 'lat.txt',
     'lon_file_name': 'lon.txt',
     'times_file_name': 'times.txt',
@@ -67,6 +29,39 @@ download_ERA5_options = {
     'east': 100.5,
     'resolution': 0.75,
 }
+
+correlations = {
+    'work_dir': download_ERA5_options['work_dir'],
+    'input_file_name': download_ERA5_options['res_cube_land_masked_and_preproc_file_name'],
+    'input_var_name': 'arr_0',
+    'output_correlation_file_name': 'corr_online_' + 'land_masked_and_preproc' + '_window_10d_delay_0d.npy',
+    'delay_time': 0,
+    'window_size': 80,
+    'num_threads': 85,
+    'need_save': False,
+    'num_parts': 666,
+    'id_part': 1,
+    'output_level': 1,
+}
+
+metrics = {
+    'work_dir': correlations['work_dir'],
+    'output_metrics_dir': 'metrics_corr_' + 'land_masked_and_preproc' + '_window_10d_delay_0d',
+    'metric_names_file_name': 'metric_names.npy',
+}
+
+network_metrics = {
+    'num_threads': 84,
+    'work_dir': correlations['work_dir'] / metrics['output_metrics_dir'],
+    'output_network_metrics_dir': Path('network_metrics'),
+}
+
+diff_metrics = {
+    'num_threads': 2,
+    'work_dir': correlations['work_dir'] / metrics['output_metrics_dir'],
+    'output_diff_metrics_dir': Path('diff_metrics'),
+}
+
 
 cyclones_info = {
     'cyclones_file_name': 'tropical_cyclones_data_1982_2020.csv',
---
+++ src/pipeline/config_project.py
@@ -0,0 +1,2 @@
+from pathlib2 import Path
+project_data_dir = Path('D:/Work/data/')
