4e14c068a268cd24e425fb22f6f3678be4a5674b
VershininaOlga <olya.olya-ver@yandex.ru>
2021-06-15 13:43:30 +0300

    Added legend to charts

--- src/plot_network_metrics/plot_cyclone_metrics.py
+++ src/plot_network_metrics/plot_cyclone_metrics.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def plot_local_grid_metric(cur_cyclone, metric_name, image_path):
     cur_times = pd.to_datetime(cur_cyclone['times']).values
     cur_metric = np.array(cur_cyclone['metrics'])
@@ -9,11 +10,12 @@ def plot_local_grid_metric(cur_cyclone, metric_name, image_path):
     cur_stds = np.array(cur_cyclone['stds'])
     
     import matplotlib.dates as mdates
-    fig = plt.figure(figsize = (7, 4), dpi = 200)
+    fig = plt.figure(figsize=(7, 4), dpi=200)
     ax = plt.gca()
-    plt.plot(cur_times, cur_metric, label = 'Metric')
-    plt.plot(cur_times, cur_means, label = 'Mean')
-    plt.fill_between(cur_times, cur_means + cur_stds, cur_means - cur_stds, color = 'b', alpha = 0.15)
+    plt.plot(cur_times, cur_metric, label='Metric')
+    plt.plot(cur_times, cur_means, label='Mean')
+    plt.fill_between(cur_times, cur_means + cur_stds, cur_means - cur_stds, color='b', alpha=0.15)
+    plt.legend(loc='upper left')
 
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%b-%d %H:%M'))
     
