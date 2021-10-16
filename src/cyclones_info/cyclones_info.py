9df1f33ad6a819c33cc53672f9a8946a72f39b6e
VershininaOlga <olya.olya-ver@yandex.ru>
2021-10-16 19:36:16 +0300

    Fixed full_extended_df_for_cyclone function

--- src/cyclones_info/cyclones_info.py
+++ src/cyclones_info/cyclones_info.py
@@ -113,12 +113,15 @@ def extension_df_for_cyclone(df):
 def full_extended_df_for_cyclone(extended_df):
     res_df = extended_df.copy()
     df_k = get_only_known_data(extended_df)
-    times, lons, lats = get_times_and_positions_for_unknown_points(extended_df, df_k)
-    for i in range(0, len(times)):
-        d = times[i]
-        row_ind = res_df[(res_df['Date (DD/MM/YYYY)'] == d.strftime('%d/%m/%Y')) &
-               (res_df['Time (UTC)'] == d.strftime('%H%M'))].index[0]
-        res_df.loc[row_ind, ['Latitude (lat.)', 'Longitude (lon.)']] = [lats[i], lons[i]]
+    if not df_k.empty:
+        times, lons, lats = get_times_and_positions_for_unknown_points(extended_df, df_k)
+        for i in range(0, len(times)):
+            d = times[i]
+            row_ind = res_df[(res_df['Date (DD/MM/YYYY)'] == d.strftime('%d/%m/%Y')) &
+                   (res_df['Time (UTC)'] == d.strftime('%H%M'))].index[0]
+            res_df.loc[row_ind, ['Latitude (lat.)', 'Longitude (lon.)']] = [lats[i], lons[i]]
+    else:
+        res_df = pd.DataFrame()
     return res_df
 
 
