c0df0deaaf0f0e43426f8e25f484264d3e3a598c
VershininaOlga <olya.olya-ver@yandex.ru>
2021-11-19 23:19:45 +0300

    Changed the type of n_greater array elements

--- src/metrics_probability/metrics_probability.py
+++ src/metrics_probability/metrics_probability.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def compute_greater(metric_array):
     inds = np.argsort(-metric_array)
-    n_greater = np.zeros(len(metric_array), dtype='float32')
+    n_greater = np.zeros(len(metric_array), dtype='int32')
     n_greater[inds] = np.arange(len(metric_array))
     return n_greater
 
