91de8eb4ccf012bb05e9343d719e9f35a59851ab
Mikhail Krivonosov <mike_live@mail.ru>
2023-03-16 17:08:23 +0300

    Added Metric store class

--- src/metric_store/__init__.py
+++ src/metric_store/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ['add_metric', 'get_metric_names', 'save_metric', 'save_metrics', 'load_metric', 'load_metrics']
-from .metric_store import add_metric, get_metric_names, save_metric, save_metrics, load_metric, load_metrics
+__all__ = ['add_metric', 'get_metric_names', 'save_metric', 'save_metrics', 'load_metric', 'load_metrics', 'Metrics']
+from .metric_store import add_metric, get_metric_names, save_metric, save_metrics, load_metric, load_metrics, Metrics
--- src/metric_store/metric_store.py
+++ src/metric_store/metric_store.py
@@ -54,3 +54,20 @@ def load_metrics(config, prefix = ''):
         metric = load_metric(config, metric_name)
         metrics[metric_name] = metric
     return metrics
+
+class Metrics():
+    def __init__(self, config, prefix=[], metric_names=None):
+        self.config = config
+        self.prefix = prefix
+        self.metric_names = metric_names
+
+        prefix_metric_names = list(get_metric_names(config, prefix).keys())
+        self.selected_metric_names = list(set(metric_names + prefix_metric_names))
+    
+    def __iter__(self):
+        for metric_name in self.selected_metric_names:
+            self.config.metrics_plot_options['metric_name'] = metric_name
+            metric = load_metric(self.config, metric_name)
+            yield metric_name, metric
+        return self
+    
