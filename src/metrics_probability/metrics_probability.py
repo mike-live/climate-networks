a19c608698408133efaf910ea17c7773260c18b7
Mikhail Krivonosov <mike_live@mail.ru>
2023-03-16 18:54:17 +0300

    Metrics probability enhanced

--- src/metrics_probability/metrics_probability.py
+++ src/metrics_probability/metrics_probability.py
@@ -1,25 +1,41 @@
 import numpy as np
 
 
-def compute_greater(metric_array):
-    inds = np.argsort(-metric_array)
-    n_greater = np.zeros(len(metric_array), dtype='int32')
-    n_greater[inds] = np.arange(len(metric_array))
+def compute_greater(train_metric, test_metric=None):
+    if test_metric is None:
+        inds = np.argsort(-train_metric)
+        n_greater = np.zeros(len(train_metric), dtype='int32')
+        n_greater[inds] = np.arange(len(train_metric))
+    else:
+        n_lower = np.searchsorted(
+            np.sort(train_metric), 
+            test_metric, 
+            side='right'
+        )
+        n_greater = len(train_metric) - n_lower
+        
     return n_greater
 
 
-def compute_probability_for_metrics(metric):
+def compute_probability_for_metrics(train_metric, test_metric=None):
     # metric - 3D np.ndarray (lat, lon, time)
 
-    n, m, k = metric.shape
+    n, m, k = train_metric.shape
     prob = np.zeros((n, m, k), dtype='float16')
-    nan_mask = np.isnan(metric)
+    nan_mask = np.isnan(train_metric)
+    is_test_metric = not test_metric is None
+    if is_test_metric:
+        nan_mask &= np.isnan(test_metric)
 
     for lat in range(n):
         for lon in range(m):
-            metric_in_node = metric[lat, lon, :]
-            n_all = len(metric_in_node)
-            n_greater = compute_greater(metric_in_node)
+            train_metric_in_node = train_metric[lat, lon, :]
+            if is_test_metric:
+                test_metric_in_node = test_metric[lat, lon, :]
+                n_greater = compute_greater(train_metric_in_node, test_metric_in_node)
+            else:
+                n_greater = compute_greater(train_metric_in_node)
+            n_all = len(train_metric_in_node)
             prob[lat, lon, :] = n_greater / n_all
     prob[nan_mask] = np.nan
 
